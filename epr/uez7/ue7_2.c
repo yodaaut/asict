01a21d77731b4a5c64e5fb86501b08d756b89f02
Philipp Panzer <4118161+yodaaut@users.noreply.github.com>
2014-01-15 13:33:27 +0100

    Create ue7_2.c

---
+++ epr/uez7/ue7_2.c
@@ -0,0 +1,31 @@
+/*
+ * Filename: ue7_2.c
+ * Autor: yodaaut
+ * Date: 01.2014
+ *
+ * Description: Run-length encoding
+ */
+ 
+#include <stdio.h>
+
+double encode_runlength(char *s);
+int decode_runlength(char *s);
+
+int main(int argc, char argv[]){
+  
+  
+  return 0;
+}
+
+
+double encode_runlength(char *s){
+  
+  
+  return 0.;
+}
+
+int decode_runlength(char *s){
+  
+  
+  return 0;
+}
