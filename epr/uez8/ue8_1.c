cab9fa2b332ae6b9398b6833d18de3d21fefbe2c
Philipp Panzer <4118161+yodaaut@users.noreply.github.com>
2014-01-16 13:13:16 +0100

    added final functions

--- epr/uez8/ue8_1.c
+++ epr/uez8/ue8_1.c
@@ -1,6 +1,6 @@
 /*
  * Filename: ue8_1.c
- * Autor: yodaaut
+ * Autor: s1330003007
  * Date: 01.2014
  *
  * Description: Pangram
@@ -9,8 +9,41 @@
 
 #include <stdio.h>
 
-//main
-int main(void){
+int check_pangramm(char *s);
+
+int main(int argc, char *argv[]){
   
+  if(argc!=2){
+    printf("Zu wenig oder zu viele Parameter uebergeben!\n");
+    return 1;
+  }
+  check_pangramm(argv[1]) ? printf("WAHR\n"): printf("FALSCH\n");
   return 0;
 }
+
+int check_pangramm(char *s){
+  int i;
+  int letters[26];
+  for(i=0;*(s+i) != 0; i++){
+    if(*(s+i)>='A'&&*(s+i)<='Z'){
+      *(s+i)+=32;
+    }
+
+  }
+  for(i=0;*(s+i) != 0; i++){
+    if(*(s+i)==' '){
+      continue;
+    }
+    *(letters+(*(s+i)-97))=1;
+
+  }
+  for(i=0;i<26;i++){
+    if(*(letters+i)!=1){
+      return 0;
+    }
+  }
+
+
+  return 1;
+}
+
--- epr/uez8/ue8_2.c
+++ epr/uez8/ue8_2.c
@@ -1,6 +1,6 @@
 /*
  * Filename: ue8_2.c
- * Autor: yodaaut
+ * Autor: s1330003007
  * Date: 01.2014
  *
  * Description: frequency analysis
@@ -8,12 +8,55 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
-//main
-int main(int argc, char argv[]){
+void frequency(char *s);
+void print(int *output);
+
+int main(int argc, char *argv[]){
   if(argc<=1){
     printf("Zu wenig Parameter uebergeben!\n");
     return 1;
   }
+  frequency(argv[1]);
   return 0;
+}
+
+void frequency(char *s){
+  int i;
+  int *letters;
+  letters = (int*) calloc(26, sizeof(int));
+  if(letters==NULL){
+    printf("Es konnte nicht genug Speicherplatz bereitgestellt werden!\n");
+    return;
+  }
+
+  for(i=0;*(s+i) != 0; i++){
+    if(*(s+i)>='A'&&*(s+i)<='Z'){
+      *(s+i)+=32;
+    }
+  }
+  for(i=0;*(s+i) != 0; i++){
+    if(!(*(s+i)>='a'&&*(s+i)<='z')){
+      continue;
+    }
+    *(letters+(*(s+i)-97))+=1;
+  }
+  print(letters);
+  free(letters);
+}
+
+void print(int *output){
+ int i, count=0;
+ for(i=0;i<26;i++){
+   count+=*(output+i);
+ }
+ printf("Character count: %d (100%c)\n", count, 37);
+ printf("Detailled Frequence Analysis:\n");
+ printf("  : count : percentage\n");
+ printf("======================\n");
+ for(i=0;i<26;i++){
+   printf("%c :%6d :\t %5.2f\n", i+97, *(output+i), 100./count*output[i]);
+ }
+
 }
